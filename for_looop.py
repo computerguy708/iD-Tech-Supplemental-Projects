daa524c204ca16e9a543266e1addc9dbf6cf1017
Tony An <tan1@hawk.iit.edu>
2018-06-17 14:20:30 -0500

    Add files via upload

---
+++ for_looop.py
@@ -0,0 +1,8 @@
+
+number_of_leaves = 14
+
+for x in range(0, number_of_leaves):
+    print("A leaf fell to the ground " +
+    str(x) + " leaves have fallen.")
+
+print("All the leaves fell. For loop complete.")
